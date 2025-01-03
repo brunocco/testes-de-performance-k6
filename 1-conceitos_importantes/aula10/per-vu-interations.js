091798323cc4846f3fa20c128983c9b5a2fac8e3
Bruno Cesar <bruno_cco@hotmail.com>
2025-01-03 15:03:58 -0300

    per-vu-iterations
    executor per-vu-iterations

---
+++ 1-conceitos_importantes/aula10/per-vu-interations.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  scenarios: {
+    contacts: {
+      executor: 'per-vu-iterations',
+      vus: 10,
+      iterations: 20,
+      maxDuration: '30s',
+    },
+  },
+};
+
+export default function () {
+  http.get('https://test.k6.io/contacts.php');
+  sleep(0.5);
+}
+
+//cada vu executa um numero fixo de iterações
+//Número total de interações = número de vus * número de iterações
