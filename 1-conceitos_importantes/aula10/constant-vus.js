24e8cc129bcd6d5b6621301b741b1533cd7df527
Bruno Cesar <bruno_cco@hotmail.com>
2025-01-03 15:04:16 -0300

    constant-vus
    executor constant-vus

---
+++ 1-conceitos_importantes/aula10/constant-vus.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  scenarios: {
+    contacts: {
+      executor: 'constant-vus',
+      vus: 10,
+      duration: '30s',
+    },
+  },
+};
+
+export default function () {
+  http.get('https://test.k6.io/contacts.php');
+  sleep(0.5);
+}
+
+//Um numero fixo de vus executa quantas interacoes forem possiveis.
